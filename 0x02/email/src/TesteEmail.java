4681fdbe68afc97e7cf953460cf9d19083f5f073
Yuri Freitas <iruytj@gmail.com>
2022-03-09 11:08:03 -0300

    task

--- 0x02/email/src/TesteEmail.java
+++ 0x02/email/src/TesteEmail.java
@@ -4,22 +4,21 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class TesteEmail {
-    private static final Pessoa pessoa = new Pessoa();
     @Test
     void testar_email_com_arroba(){
                 assertTrue(
-                        pessoa.emailValid("email_teste@dominio.com.br"));
+                        Pessoa.emailValid("email_teste@dominio.com.br"));
     }
 
     @Test
     void testar_email_sem_arroba(){
             assertFalse(
-                    pessoa.emailValid("email_testedominio.com.br"));
+                    Pessoa.emailValid("email_testedominio.com.br"));
     }
 
     @Test
     void testar_email_mais_50_caracteres(){
             assertEquals(false,
-                    pessoa.emailValid("emailtestemuitolongonaodeveser_valido@dominio.com.br"));
+                    Pessoa.emailValid("emailtestemuitolongonaodeveser_valido@dominio.com.br"));
     }
 }
