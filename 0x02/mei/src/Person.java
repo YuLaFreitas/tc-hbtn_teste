3eb9cb4e85e3925ad6814741b7bc0c10745e4b17
Yuri Freitas <iruytj@gmail.com>
2022-03-09 15:33:39 -0300

    TASK

---
+++ 0x02/mei/src/Person.java
@@ -0,0 +1,90 @@
+import java.util.Date;
+
+public class Person {
+
+    private String name;
+    private String surname;
+    private Date bithDate;
+    private boolean anotherCompanyOwner = false;
+    private boolean Pensioner = false;
+    private boolean PublicServer = false;
+    float salary;
+
+    public String fullname(){
+        return String.format("%s %s", this.name, this.surname);
+    }
+
+    public float calculateYearlySalary(){
+        return getSalary() * 12;
+    }
+
+    public boolean isMEI(){
+        int age = new Date().getYear() - this.bithDate.getYear();
+        if(
+                (getSalary()*12) < 130000 &&
+                age > 18 &&
+                anotherCompanyOwner &&
+                Pensioner &&
+                PublicServer){
+                    return true;
+        }
+        return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getBithDate() {
+        return bithDate;
+    }
+
+    public void setBithDate(Date bithDate) {
+        this.bithDate = bithDate;
+    }
+
+    public boolean isAnotherCompanyOwner() {
+        return anotherCompanyOwner;
+    }
+
+    public void setAnotherCompanyOwner(boolean anotherCompanyOwner) {
+        this.anotherCompanyOwner = anotherCompanyOwner;
+    }
+
+    public boolean isPensioner() {
+        return Pensioner;
+    }
+
+    public void setPensioner(boolean pensioner) {
+        Pensioner = pensioner;
+    }
+
+    public boolean isPublicServer() {
+        return PublicServer;
+    }
+
+    public void setPublicServer(boolean publicServer) {
+        PublicServer = publicServer;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+}
