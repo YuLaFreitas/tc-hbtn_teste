6f977f13021012a8eb597b784f4c945e69db6909
Yuri Freitas <iruytj@gmail.com>
2022-03-10 09:03:15 -0300

    task

--- 0x02/mei/src/PersonTest.java
+++ 0x02/mei/src/PersonTest.java
@@ -1,5 +1,3 @@
-package src;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
